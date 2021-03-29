d777fe64dd062d19469d25ec10fcfa5af18c9987
Clark Joy Pesco <siezmicrush@gmail.com>
2021-03-30 07:18:41 +0800

    Splash Screen

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:demoinsta/blocs/blocs.dart';
+import 'package:demoinsta/screens/screens.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SplashScreen extends StatelessWidget {
+  static const String routeName = '/splash';
+
+  static Route route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => SplashScreen(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state.status == AuthStatus.unauthenticated) {
+            //Go to the login Screen.
+            Navigator.of(context).pushNamed(LoginScreen.routeName);
+          } else if (state.status == AuthStatus.authenticated) {
+            // Go to the new Nav Screen.
+            Navigator.of(context).pushNamed(NavScreen.routeName);
+          }
+        },
+        child: const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      ),
+    );
+  }
+}
