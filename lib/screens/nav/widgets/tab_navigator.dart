4ef44c445efc721088a32f3b6d9f984b15c5ca16
Clark Joy Pesco <siezmicrush@gmail.com>
2021-03-31 09:59:40 +0800

    Profile Screen

--- lib/screens/nav/widgets/tab_navigator.dart
+++ lib/screens/nav/widgets/tab_navigator.dart
@@ -1,7 +1,11 @@
+import 'package:demoinsta/blocs/blocs.dart';
 import 'package:demoinsta/config/custom_router.dart';
 import 'package:demoinsta/enums/enums.dart';
+import 'package:demoinsta/repositories/repositories.dart';
+import 'package:demoinsta/screens/profile/bloc/profile_bloc.dart';
 import 'package:demoinsta/screens/screens.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TabNavigator extends StatelessWidget {
   static const String tabNavigatorRoot = '/';
@@ -47,7 +51,14 @@ class TabNavigator extends StatelessWidget {
       case BottomNavItem.notifications:
         return NotificationsScreen();
       case BottomNavItem.profile:
-        return ProfileScreen();
+        return BlocProvider<ProfileBloc>(
+          create: (_) => ProfileBloc(
+            userRepository: context.read<UserRepository>(),
+            authBloc: context.read<AuthBloc>(),
+          )..add(
+              ProfileLoadUser(userId: context.read<AuthBloc>().state.user.uid)),
+          child: ProfileScreen(),
+        );
       default:
         return Scaffold();
     }
---
+++ lib/screens/profile/widgets/profile_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ProfileButton extends StatelessWidget {
+  final bool isCurrentUser;
+  final bool isFollowing;
+  const ProfileButton({
+    Key key,
+    @required this.isCurrentUser,
+    @required this.isFollowing,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return isCurrentUser
+        ? TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              primary: Colors.white,
+              backgroundColor: Theme.of(context).primaryColor,
+              textStyle: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            child: const Text('Edit Profile'),
+          )
+        : TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: isFollowing
+                  ? Colors.grey[300]
+                  : Theme.of(context).primaryColor,
+              primary: isFollowing ? Colors.black : Colors.white,
+              textStyle: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            child: Text(
+              isFollowing ? 'Unfollow' : 'Follow',
+            ),
+          );
+  }
+}
